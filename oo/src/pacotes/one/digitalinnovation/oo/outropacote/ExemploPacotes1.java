0cf66953400128284749b2b981de05029d7e31fb
Seu nome <priscilasolla.ps@gmail.com>
2022-11-05 17:43:08 -0300

    final

---
+++ oo/src/pacotes/one/digitalinnovation/oo/outropacote/ExemploPacotes1.java
@@ -0,0 +1,5 @@
+package pacotes.one.digitalinnovation.oo.outropacote;
+
+public class ExemploPacotes1 {
+    
+}
---
+++ oo/src/pacotes/qualquer/outro/pacote/ExemplosPacotes3.java
@@ -0,0 +1,11 @@
+package pacotes.qualquer.outro.pacote;
+
+import pacotes.one.digitalinnovation.oo.outropacote.ExemploPacotes1;
+
+/**
+ * Classe de exemplo para o exercício da Aula 5 de Orientação a Objetos.
+ */
+class ExemplosPacotes3 {
+
+    ExemploPacotes1 exemploPacotes1;
+}
